726dbf78443ae48ae4dd1ee98160989813cf6828
chaart13 <chaart13@gmail.com>
2024-10-24 16:53:34 +0200

    fix: error handling

--- srv/city-service.js
+++ srv/city-service.js
@@ -2,10 +2,10 @@ const cds = require("@sap/cds");
 const papaparse = require("papaparse");
 
 module.exports = function () {
-  this.on("importCities", (req) => {
+  this.on("importCities", async (req) => {
     try {
       let forImport = {};
-      const decodedContent = atob(req.data.fileContent);
+      const decodedContent = _decodePayload(req.data.fileContent);
       switch (req.data.mimeType) {
         case "application/json":
           forImport = {
@@ -20,28 +20,48 @@ module.exports = function () {
           };
           break;
         default:
-          throw new Error("Not supported type");
+          throw new CustomError("Unsupported file type");
       }
 
-      return INSERT.into(forImport.table).entries(forImport.data);
+      const res = await INSERT.into(forImport.table).entries(forImport.data);
+      return res;
     } catch (err) {
-      req.error(400, err);
+      req.error(
+        400,
+        err.name === errorName ? err : "The file format is incompatible"
+      );
     }
   });
 };
+
+const errorName = "Custom";
+class CustomError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = errorName;
+  }
+}
 
 function _parseJSON(data) {
   try {
     return JSON.parse(data);
   } catch {
-    throw new Error("Error parsing JSON");
+    throw new CustomError("Error parsing JSON");
   }
 }
 
 function _parseCSV(data) {
   const parsed = papaparse.parse(data, { header: true });
   if (parsed.errors.length) {
-    throw new Error("Error parsing CSV");
+    throw new CustomError("Error parsing CSV");
   }
   return parsed.data;
+}
+
+function _decodePayload(data) {
+  try {
+    return atob(data);
+  } catch {
+    throw new CustomError("Error decoding payload");
+  }
 }
